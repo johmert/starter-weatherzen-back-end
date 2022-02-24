35f4464ecac8929108d823668f24afc175a48635
johmert <johmert@users.noreply.github.com>
2022-02-23 17:59:11 -0800

    updated backend on POST

---
+++ src/observations/observations.controller.js
@@ -0,0 +1,16 @@
+let nextId = 1;
+
+async function create(req, res) {
+    const newObservation = req.body.data;
+    const now = new Date().toISOString();
+
+    newObservation.observation_id = nextId++;
+    newObservation.created_at = now;
+    newObservation.updated_at = now;
+
+    res.status(201).json({ data: newObservation });
+}
+
+module.exports = {
+    create,
+}
